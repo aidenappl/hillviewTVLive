bb01933feb04a89e6e515975915aa10cd00d8caa
Aiden Appleby <aiden.appleby3@gmail.com>
2021-12-07 18:43:22 -0800

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/watch',
+    pathMatch: 'full'
+  },
+  {
+    path: 'watch',
+    loadChildren: () =>
+      import('./public/watch/watch.module').then((m) => m.WatchModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
---
+++ src/app/public/watch/watch.component.ts
@@ -0,0 +1,39 @@
+import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
+
+import videojs from 'video.js';
+
+@Component({
+  selector: 'app-watch',
+  templateUrl: './watch.component.html',
+  styleUrls: ['./watch.component.scss']
+})
+export class WatchComponent implements OnInit {
+
+  @ViewChild('target', { static: true }) target!: ElementRef;
+  // see options: https://github.com/videojs/video.js/blob/maintutorial-options.html
+  @Input() options!: {
+      fluid: boolean,
+      aspectRatio: string,
+      autoplay: boolean,
+      sources: {
+          src: string,
+          type: string,
+      }[],
+  };
+  player!: videojs.Player;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.player = videojs(this.target.nativeElement, this.options, function onPlayerReady() {
+      console.log('onPlayerReady', this);
+    });
+  }
+  ngOnDestroy() {
+    // destroy player
+    if (this.player) {
+      this.player.dispose();
+    }
+  }
+
+}
