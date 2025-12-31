7dc7a3fc1a98691225ee01c79294dbd3ce9dd78d
Aiden Appleby <aiden.appleby3@gmail.com>
2025-12-31 15:07:47 -0500

    code cleanup

--- src/app/public/watch/watch.component.ts
+++ src/app/public/watch/watch.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class WatchComponent implements OnInit {
   showSplash = true;
 
-  constructor() {}
+  constructor() { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 }
